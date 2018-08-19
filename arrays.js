320c2b69f7412acff74cea634b5c72bd17e4c619
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-19 13:56:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(chocolateBars,CandyString){
+return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars,candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;}
+
+  function addElementToBeginningOfArray(chocolateBars,candyString){
+    array.unshift(candyString);
+    console.log(array);
+    return chocolateBars;
+  }
+
+function accessElementInArray(array,index){
+  return array[index];
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+return chocolateBars;
+}
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars.slice();
+  return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
